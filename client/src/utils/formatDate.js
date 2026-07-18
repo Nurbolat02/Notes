47750e3910c717da0de220744d25bdd33d77ac61
Nurbolat02 <79019914+Nurbolat02@users.noreply.github.com>
2026-07-18 20:32:44 +0500

    fix two more  mistake

--- client/src/utils/formatDate.js
+++ client/src/utils/formatDate.js
@@ -12,13 +12,13 @@
 // - minutes — минуты с ведущим нулём
 //
 // Возвращает строку, собранную из day, month, year, hours, minutes
-function formatDate(isoString){
-    const date = new Date(isoString),
-    day = toString(date.getDay).padStart(2, '0'),
-    month = toString(date.getMonth+1).padStart(2,'0'),
-    year = toString(date.getFullYear),
-    hours = toString(date.getHours).padStart(2,'0'),
-    minutes = toString(date.getMinutes).padStart(2,'0');
+function formatDate(isoString) {
+    const date = new Date(isoString)
+    const day = date.getDate().toString().padStart(2, '0')
+    const month = (date.getMonth() + 1).toString().padStart(2, '0')
+    const year = date.getFullYear().toString()
+    const hours = date.getHours().toString().padStart(2, '0')
+    const minutes = date.getMinutes().toString().padStart(2, '0')
     return `${day}.${month}.${year} ${hours}:${minutes}`
 }
 export default formatDate
